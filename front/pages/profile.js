9a1c9aac86397fc95598770f72f8e29b0fb30e31
kdo0129 <eha0129@naver.com>
2020-06-03 14:23:36 +0900

    Change : structure

---
+++ front/pages/profile.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AppLayout from '../components/AppLayout';
+
+const profile = () => {
+	return <AppLayout>profile</AppLayout>;
+};
+
+export default profile;
---
+++ front/pages/signup.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import AppLayout from '../components/AppLayout';
+const signup = () => {
+	return <AppLayout>signup</AppLayout>;
+};
+
+export default signup;
